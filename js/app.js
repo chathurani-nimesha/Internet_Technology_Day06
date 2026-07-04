d37b112ee150874158bc7d0fe2d908a1f093010f
Chathurani Nimesha <chathunimesha389@gmail.com>
2026-07-04 10:50:44 +0530

    object creation

--- js/app.js
+++ js/app.js
@@ -4,33 +4,56 @@ console.log("Hi... Day 06 of Java Script");
 
 //-----------------------------------------------
 // object -01 method ==using a class
-class Customer{
-    name;
-    age;
-    address;
+class Customer {
+  name;
+  age;
+  address;
 
-    constructor(name,age,address){
-        this.name=name;
-        this.age=age;
-        this.address=address;
-    }
+  constructor(name, age, address) {
+    this.name = name;
+    this.age = age;
+    this.address = address;
+  }
 }
-let customer1=new Customer("Chathu",20,"Anuradhapura");
+let customer1 = new Customer("Chathu", 20, "Anuradhapura");
 console.log(customer1);
 console.log(customer1.name);
 
 //object -02 method
 
-let customer2={
-    name: "John",
-    age:30,                //seperate lines using comma(,)
-    address:"New York" ,
-    items: ["item1","item2"],   //in js, object is initialized using : (colon).
-    father:{    //another object inside the cusotmer ojbect
-        name: "Deo",
-        age: 10,
-        address: "Kandy"
-    }
+let customer2 = {
+  name: "John",
+  age: 30, //seperate lines using comma(,)
+  address: "New York",
+  items: ["item1", "item2"], //in js, object is initialized using : (colon).
+  books: [
+    {
+      id: 1001,
+      name: "Book1",
+      variants: [
+        {
+            id:1,
+            name:"variant1",
+            price:10
+        },
+        {
+            id:2,
+            name:"variants2",
+            price:20
+        }
+      ]
+    },
+    {
+      id: 1002,
+      name: "Book2",
+    },
+  ],
+  father: {
+    //another object inside the cusotmer ojbect
+    name: "Deo",
+    age: 10,
+    address: "Kandy",
+  },
 };
 
 console.log(customer2);
@@ -43,5 +66,9 @@ console.log(customer2.address);
 console.log(customer2.father.name);
 console.log(customer2.father.address);
 
-//------------------------------------------------
+//console.log(customer2.books.variants.id);
+
+//----------------------------------------------------------
+//      Document Object Model (DOM) Manipulation
+//----------------------------------------------------------
 
